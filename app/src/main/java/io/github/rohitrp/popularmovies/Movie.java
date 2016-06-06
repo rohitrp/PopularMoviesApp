f66d4827ca6abdc0b949603aae9182e4794a2f17
Rohit Prasad <rohitrp3@gmail.com>
2016-06-06 21:01:06 +0530

    [feat] Parse movies from the JSON received by the API call

--- app/src/main/java/io/github/rohitrp/popularmovies/Movie.java
+++ app/src/main/java/io/github/rohitrp/popularmovies/Movie.java
@@ -4,16 +4,19 @@ public class Movie {
     private String mTitle;
     private String mPosterUrl;
     private String mSynopsis;
-    private String mRatings;
+    private double mRatings;
     private String mReleaseDate;
+    private boolean mIsAdult;
 
     public Movie(String title, String posterUrl,
-                 String synopsis, String ratings, String releaseDate) {
+                 String synopsis, double ratings, String releaseDate,
+                 boolean isAdult) {
         this.mTitle = title;
         this.mPosterUrl = posterUrl;
         this.mSynopsis = synopsis;
         this.mRatings = ratings;
         this.mReleaseDate = releaseDate;
+        this.mIsAdult = isAdult;
     }
 
     public String getTitle() {
@@ -28,11 +31,15 @@ public class Movie {
         return mSynopsis;
     }
 
-    public String getRatings() {
+    public double getRatings() {
         return mRatings;
     }
 
     public String getmReleaseDate() {
         return mReleaseDate;
+    }
+
+    public boolean isAdult() {
+        return mIsAdult;
     }
 }
--- app/src/main/java/io/github/rohitrp/popularmovies/MovieFragment.java
+++ app/src/main/java/io/github/rohitrp/popularmovies/MovieFragment.java
@@ -12,6 +12,10 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
@@ -52,7 +56,7 @@ public class MovieFragment extends Fragment {
         int id = item.getItemId();
 
         if (id == R.id.menu_refresh) {
-            new FetchMovies().execute("popular");
+            new FetchMovies().execute("top_rated");
             return true;
         }
 
@@ -68,7 +72,7 @@ public class MovieFragment extends Fragment {
             HttpURLConnection urlConnection = null;
             BufferedReader reader = null;
 
-            String movieJsonStr = null;
+            String moviesJsonStr = null;
 
             try {
                 final String TMDB_BASE_URL =
@@ -81,6 +85,8 @@ public class MovieFragment extends Fragment {
                         .build();
 
                 URL url = new URL(builtUri.toString());
+
+                Log.d(LOG_TAG, builtUri.toString());
 
                 urlConnection = (HttpURLConnection) url.openConnection();
                 urlConnection.setRequestMethod("GET");
@@ -105,15 +111,78 @@ public class MovieFragment extends Fragment {
                     return null;
                 }
 
-                movieJsonStr = buffer.toString();
-
-                Log.d(LOG_TAG, movieJsonStr);
+                moviesJsonStr = buffer.toString();
             } catch (IOException e) {
                 Log.e(LOG_TAG, "Error ", e);
 
                 return null;
+            } finally {
+                if (urlConnection != null) {
+                    urlConnection.disconnect();
+                }
+                if (reader != null) {
+                    try {
+                        reader.close();
+                    } catch (final IOException e) {
+                        Log.e(LOG_TAG, "Error closing stream", e);
+                    }
+                }
             }
-            return new Movie[0];
+
+            try {
+                return getMoviesDataFromJson(moviesJsonStr);
+            } catch (JSONException e) {
+                Log.e(LOG_TAG, e.getMessage(), e);
+                e.printStackTrace();
+            }
+
+            // This will only happen if there was an error getting or parsing
+            // movies.
+            return null;
+        }
+
+        private Movie[] getMoviesDataFromJson(String moviesJsonStr)
+            throws JSONException {
+
+            final String TMDB_RESULTS = "results";
+            final String TMDB_POSTER_PATH = "poster_path";
+            final String TMDB_ADULT = "adult";
+            final String TMDB_SYNOPSIS = "overview";
+            final String TMDB_RELEASE_DATE = "release_date";
+            final String TMDB_TITLE = "original_title";
+            final String TMDB_RATINGS = "vote_average";
+            final String TMDB_BASE_POSTER_URL =
+                    "http://image.tmdb.org/t/p/";
+            final String TMDB_POSTER_SIZE = "w185";
+
+            JSONObject moviesJson = new JSONObject(moviesJsonStr);
+            JSONArray moviesArray = moviesJson.getJSONArray(TMDB_RESULTS);
+
+            Movie[] resultMovies = new Movie[moviesArray.length()];
+
+            for (int i = 0; i < moviesArray.length(); i++) {
+                String title;
+                String posterUrl;
+                String synopsis;
+                double ratings;
+                String releaseDate;
+                boolean isAdult;
+
+                JSONObject currMovie = moviesArray.getJSONObject(i);
+
+                title = currMovie.getString(TMDB_TITLE);
+                posterUrl = TMDB_BASE_POSTER_URL + TMDB_POSTER_SIZE +
+                        currMovie.getString(TMDB_POSTER_PATH);
+                synopsis = currMovie.getString(TMDB_SYNOPSIS);
+                ratings = currMovie.getDouble(TMDB_RATINGS);
+                releaseDate = currMovie.getString(TMDB_RELEASE_DATE);
+                isAdult = currMovie.getBoolean(TMDB_ADULT);
+
+                resultMovies[i] = new Movie(title, posterUrl, synopsis,
+                        ratings, releaseDate, isAdult);
+            }
+
+            return resultMovies;
         }
     }
 }
