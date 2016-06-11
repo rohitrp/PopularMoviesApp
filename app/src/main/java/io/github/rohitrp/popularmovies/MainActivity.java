e0c71094f23abb2c91aa4c056db0365afbb93951
Rohit Prasad <rohitrp3@gmail.com>
2016-06-12 00:11:06 +0530

    [feat] Add action bar in main activity

--- app/src/main/java/io/github/rohitrp/popularmovies/MainActivity.java
+++ app/src/main/java/io/github/rohitrp/popularmovies/MainActivity.java
@@ -3,6 +3,7 @@ package io.github.rohitrp.popularmovies;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -18,6 +19,9 @@ public class MainActivity extends AppCompatActivity {
                     .add(R.id.container, new MovieFragment())
                     .commit();
         }
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
     }
 
     @Override
